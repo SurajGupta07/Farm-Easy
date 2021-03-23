60933b5b7e532f530f1c8c581769112d7481a013
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-03-23 19:48:53 +0530

    Cart Functionality

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import {Checkout, CartHeader, ProductList, Cart } from '../src/components/Cart'
+
+function App() {
+  const [route, setRoute] = useState("products");
+  return (
+    <div className="App">
+      <h1 className="headerTitle">Farm Easy</h1>
+			<div className="app-body">
+				<div>
+					<button	onClick={() => setRoute("products")} className="default-button add-spacing">
+						Products
+					</button >
+          <button	onClick={() => setRoute("cart")} className="default-button add-spacing">
+						Cart
+					</button>
+					<button	onClick={() => setRoute("checkout")} className="default-button add-spacing">
+						Checkout
+					</button>
+				</div>
+				<CartHeader />
+				{ route === "checkout" && <Checkout />}
+				{route === "cart" && <Cart />}
+				{route === "products" && <ProductList />}
+			</div>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CartContext.js
@@ -0,0 +1,19 @@
+import React, { useState, useContext } from 'react';
+import {createContext} from 'react';
+
+export const CartContext = createContext();
+
+export function CartProvider({children}){
+    const [itemsInCart, setItemsInCart] = useState([])
+    return(
+        <>
+        <CartContext.Provider value={{itemsInCart, setItemsInCart}}>
+            {children}
+        </CartContext.Provider>
+        </>
+    )
+}
+
+export function useCart(){
+    return useContext(CartContext)
+}
