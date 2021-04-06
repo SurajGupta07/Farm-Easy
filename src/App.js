b8bca4165aba48b820399396f651041d26036897
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-04-06 22:28:08 +0530

    Refactored products listing

--- src/App.js
+++ src/App.js
@@ -2,28 +2,37 @@ import './App.css';
 import React from 'react';
 import {useState} from 'react';
 import {Checkout, CartHeader, ProductList, Cart } from '../src/components/Cart'
+import cartLogo from './images/cart.png';
+import homeLogo from './images/home.png';
+import wishlistLogo from './images/wishlist.png';
+
 
 function App() {
-  const [route, setRoute] = useState("products");
+  const [route, setRoute] = useState("itemsInCart");
   return (
     <div className="App">
-      <h1 className="headerTitle">Farm Easy</h1>
 			<div className="app-body">
-				<div>
-					<button	onClick={() => setRoute("products")} className="default-button add-spacing">
-						Products
-					</button >
-          <button	onClick={() => setRoute("cart")} className="default-button add-spacing">
-						Cart
-					</button>
-					<button	onClick={() => setRoute("checkout")} className="default-button add-spacing">
-						Checkout
-					</button>
+				<div className="nav-container mainNavbar">
+					<nav className="navigation container backgroundColor">
+						<h1 className="nav-brand" style={{fontSize:"1.5rem"}}>Farm Easy</h1>
+						<div className="non-bullet nav-pills">
+							<button	onClick={() => setRoute("itemsInCart")} className="buttonLogo">
+								<img src={homeLogo} className="headerLogo" alt="Home"></img>
+							</button >
+							<button	onClick={() => setRoute("cart")} className="buttonLogo">
+								<img src = {cartLogo} className="headerLogo" alt="Cart"></img>
+							</button>
+							<button	onClick={() => setRoute("checkout")} className="buttonLogo">
+								<img src={wishlistLogo} className="headerLogo" alt="Wishlist"></img>
+							</button>
+						</div>
+  					{/* <div>Items in Cart {}</div> */}
+					</nav>
 				</div>
 				<CartHeader />
 				{ route === "checkout" && <Checkout />}
 				{route === "cart" && <Cart />}
-				{route === "products" && <ProductList />}
+				{route === "itemsInCart" && <ProductList />}
 			</div>
         
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,43 +6,57 @@ const products = [
     id: 1,
     name: 'Baby Corn',
     image: 'https://5.imimg.com/data5/YC/NA/SS/SELLER-70813520/frozen-baby-corn-cobs-500x500.jpg',
-    price: 'Rs. 50'
+    price: 'Rs. 50',
+    quantity: 0,
+    availability: 'IN STOCK'
   },
   {
     id: 2,
     name: 'Tomamto',
     image: 'https://images.unsplash.com/photo-1606588260160-0c4707ab7db5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1316&q=80',
-    price: 'Rs. 20'
+    price: 'Rs. 20',
+    quantity: 0,
+    availability: 'IN STOCK'
   },
   {
     id: 3,
     name: 'Carrot',
     image: 'https://images.unsplash.com/photo-1590868309235-ea34bed7bd7f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80',
-    price: 'Rs. 40'
+    price: 'Rs. 40',
+    quantity: 0,
+    availability: 'OUT OF STOCK'
   },
   {
     id: 4,
     name: 'Potato',
     image: 'https://images.unsplash.com/photo-1604752466673-f73b5b0daa75?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
-    price: 'Rs. 25'
+    price: 'Rs. 25',
+    quantity: 0,
+    availability: 'IN STOCK'
   },
   {
     id: 5,
     name: 'Spanich',
     image: 'https://fillyourplate.org/blog/wp-content/uploads/2020/01/Spanich-900x650.png',
-    price: 'Rs. 15'
+    price: 'Rs. 15',
+    quantity: 0,
+    availability: 'IN STOCK'
   },
   {
     id: 6,
     name: 'Lemon',
     image: 'https://images.unsplash.com/photo-1593620286877-5da58b04f3a5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    price: 'Rs. 5'
+    price: 'Rs. 5',
+    quantity: 0,
+    availability: 'OUT OF STOCK'
   },
   {
     id: 7,
     name: 'Onion',
     image: "https://images.unsplash.com/photo-1508747703725-719777637510?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-    price: 'Rs. 90'
+    price: 'Rs. 90',
+    quantity: 0,
+    availability: 'IN STOCK'
   }
 ]
  
@@ -52,6 +66,10 @@ function ShowItem({item}){
       <img src={item.image} alt="Logo" className="card-img" />
       <h2 className="card-title">{item.name}</h2>
       <p className="card-text">{item.price}</p>
+      <button>+</button>
+      {item.quantity}
+      <button>-</button>
+      <button className="default-button">Remove Item</button>
     </div>
   )
 }
@@ -80,14 +98,13 @@ export function CartHeader() {
     return <h3 className="sub-header">Items in Cart {itemsInCart.length}</h3>;
   }
 
-export function Cart(){
+export function Cart(){ 
   const {itemsInCart} = useCart();
   return <>
-  <h1 className="sub-header">Cart</h1>
+  <h1 className="main-header">Cart</h1>
   <ul>
     {itemsInCart.map((item) => (<li><ShowItem item={item} /></li>))}
   </ul>
-  {/* <Checkout /> */}
   </>
 }
 
@@ -95,13 +112,15 @@ export const ProductList = () => {
   const {setItemsInCart} = useCart();
     return(
         <div>
-          <h1 className="sub-header">Products</h1>
           {products.map((item) => (
           <div key={item.id} className="card card-body card-spacing">
             <img src={item.image} alt="Logo" className="card-img" />
             <h2 className="card-title">{item.name}</h2>
             <p className="card-text">{item.price}</p>
-            <button className="default-button" onClick={() => setItemsInCart((items) => [...items, item ])}>Add to Cart</button>
+            <p className="card-text">{item.availability}</p>
+            <button className="default-button" onClick={function(e){
+              setItemsInCart((items) => [...items, item ])
+            }}>Add to Cart</button>
           </div>
           ))}
         </div>
