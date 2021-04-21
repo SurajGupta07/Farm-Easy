6e0c6092c2b64e0a1ad490ec76a2d844427231ad
SurajGupta07 <bgs071998@gmail.com>
2021-04-21 19:17:36 +0530

    Basic Auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import {Checkout, CartHeader, ProductList, Cart } from '../src/components/Cart'
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {Checkout, CartHeader, ProductList, Cart} from '../src/components/Cart'
 import Navbar from "../src/components/Navbar"
 import Login from "../src/components/Login"
 import Signup from "../src/components/Signup"
 import ForgotPassword from "../src/components/ForgotPassword"
+import { useState } from 'react';
+
+function PrivateRoute({ login, ...props }){
+    return login ? <Route {...props} /> : <Navigate replace to="/login" /> 
+}
 
 function App() {
-  return (
-    <div className="App">
-			<div className="app-body">
-				<Navbar />
-				<CartHeader />
-				<Routes>
-        			<Route path="/" element={<ProductList />} />
-        			<Route path="cart" element={<Cart />} />
-					<Route path="checkout" element={<Checkout />} />
-					<Route path="login" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
-					<Route path="forgot-password" element={<ForgotPassword />} />
-      			</Routes>
-			</div>
-        
-    </div>
-  );
+    const [login, setLogin] = useState(false)
+    return (
+        <div className="App">
+            <div className="app-body">
+            <button onClick={() => {setLogin(login => !login)}}>{login ? "logout" : "login"}</button>
+                <Navbar/>
+                <CartHeader/>
+                <Routes>
+                    <Route path="/" element={< ProductList />}/>
+                    <Route path="cart" element={< Cart />}/>
+                    <Route path="login" element={< Login />}/> 
+                    <Route path="signup" element={< Signup />}/>
+                    <Route path="forgot-password" element={< ForgotPassword />}/>
+                    <PrivateRoute path="/checkout" login={login} element={< Checkout />}/>
+                </Routes>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <div>
             <input placeholder="Email"></input>
             <input placeholder="Password"></input>
             <input placeholder="Name"></input>
             <button>Login</button>
             <div>
-                New user? <Link to="/signup">Signup</Link>
+                New user?
+                <Link to="/signup">Signup</Link>
             </div>
             <div>
                 <Link to="/forgot-password">Forgot Password</Link>
