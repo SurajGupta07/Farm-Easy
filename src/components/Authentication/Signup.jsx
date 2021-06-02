684c943160fc97a850120af4fa82504a3169ab8f
SurajGupta07 <bgs071998@gmail.com>
2021-06-02 18:38:31 +0530

    Removing unused variables

--- src/components/Authentication/Signup.jsx
+++ src/components/Authentication/Signup.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 
 export default function Signup() {
